eaedf50d5fb8ebdc75da16bc789ed8a45ad9f02d
FarukSina <200437201@student.georgianc.on.ca>
2022-04-29 16:50:25 -0400

    Test2

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ function signHmacSha256(key, str) {
 
   let signed = hmac.update(Buffer.from(str, "utf-8")).digest("hex");
   let signed2 = hmac2.update(Buffer.from(str, "utf-8")).digest("base64");
-  let signed3 = hmac3.update(Buffer.from(str, "json")).digest("base64");
   let signed4 = hmac4.update(str).digest("hex");
   let signed5 = hmac5.update(str).digest("base64");
   console.log("signed", signed2, signed3, signed4, signed5);
